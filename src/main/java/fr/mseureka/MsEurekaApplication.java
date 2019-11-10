aa2115abc88aaad59c6e01b1a9fd9869a204fba2
karim <karimessouabni0@gmail.com>
2019-11-10 21:37:39 +0100

    init commit

---
+++ src/main/java/fr/mseureka/MsEurekaApplication.java
@@ -0,0 +1,13 @@
+package fr.mseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEurekaApplication.class, args);
+    }
+
+}
